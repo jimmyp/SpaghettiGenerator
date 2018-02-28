6126187c8a4df815ad676e281b7a56adcd327fd3
Jim Pelletier <jpelletier@liberty.com.au>
2018-02-28 16:58:34 +1100

    Generate some spaghetti code

---
+++ SpaghettiGenerator/Class1.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spaghetti
+{
+    public class Factory
+    {
+        Method _method = new Method();
+        Error _error = new Error();
+        Configuration _configuration = new Configuration();
+        Identifier _identifier = new Identifier();
+        Model _model = new Model();
+        public void Composite()
+        {
+            _method.Transaction();
+            _error.Notification();
+        }
+        public void Readable()
+        {
+            _configuration.Statement();
+            _identifier.Remote();
+        }
+        public void Multipart()
+        {
+            _model.Reflective();
+            _model.Interface();
+        }
+    }
+    public class Bean
+    {
+        Something _something = new Something();
+        SomethingElse _somethingElse = new SomethingElse();
+        Factory _factory = new Factory();
+        public void Batch()
+        {
+            _something.Identifiable();
+            _somethingElse.Stateless();
+        }
+        public void Based()
+        {
+        }
+        public void Http()
+        {
+            _factory.Multipart();
+            _something.Transactional();
+            _somethingElse.Stateless();
+        }
+    }
+    public class Wrapper
+    {
+        Factory _factory = new Factory();
+        Bean _bean = new Bean();
+        Identifier _identifier = new Identifier();
+        Method _method = new Method();
+        Property _property = new Property();
+        Something _something = new Something();
+        public void Prepared()
+        {
+        }
+        public void Advisable()
+        {
+            _factory.Readable();
+            _bean.Http();
+            _identifier.Interruptible();
+            _method.Scope();
+        }
+        public void Iterable()
+        {
+            _property.Meta();
+            _identifier.Remote();
+            _identifier.Invalid();
+            _something.Transactional();
+        }
+    }
+    public class Visitor
+    {
+        Visitor _visitor = new Visitor();
+        Wrapper _wrapper = new Wrapper();
+        Identifier _identifier = new Identifier();
+        Error _error = new Error();
+        Configuration _configuration = new Configuration();
+        Method _method = new Method();
+        public void Autowire()
+        {
+            _visitor.Xml();
+            _wrapper.Iterable();
+        }
+        public void Principal()
+        {
+            _identifier.Common();
+            _error.Focus();
+        }
+        public void Session()
+        {
+            _identifier.Traversal();
+            _configuration.Supported();
+            _wrapper.Iterable();
+            _method.Literal();
+        }
+        public void Xml()
+        {
+        }
+    }
+    public class Model
+    {
+        Value _value = new Value();
+        Visitor _visitor = new Visitor();
+        Identifier _identifier = new Identifier();
+        Error _error = new Error();
+        public void Reflective()
+        {
+            _value.Simple();
+            _visitor.Xml();
+        }
+        public void Interface()
+        {
+            _identifier.Driven();
+            _visitor.Xml();
+            _error.Focus();
+        }
+        public void Observable()
+        {
+        }
+    }
+    public class Singleton
+    {
+        Configuration _configuration = new Configuration();
+        public void Abstract()
+        {
+            _configuration.Aware();
+        }
+        public void Data()
+        {
+        }
+        public void Failure()
+        {
+        }
+    }
+    public class Method
+    {
+        Method _method = new Method();
+        Visitor _visitor = new Visitor();
+        Error _error = new Error();
+        Identifier _identifier = new Identifier();
+        Wrapper _wrapper = new Wrapper();
+        public void Transaction()
+        {
+            _method.Literal();
+            _visitor.Autowire();
+            _error.Notification();
+        }
+        public void Jms()
+        {
+            _identifier.Type();
+        }
+        public void Literal()
+        {
+        }
+        public void Scope()
+        {
+            _identifier.Remote();
+            _visitor.Session();
+            _wrapper.Iterable();
+        }
+    }
+    public class Configuration
+    {
+        Visitor _visitor = new Visitor();
+        Identifier _identifier = new Identifier();
+        Wrapper _wrapper = new Wrapper();
+        Property _property = new Property();
+        Something _something = new Something();
+        Error _error = new Error();
+        Model _model = new Model();
+        Singleton _singleton = new Singleton();
+        public void Supported()
+        {
+            _visitor.Principal();
+            _identifier.Type();
+            _wrapper.Iterable();
+        }
+        public void Aware()
+        {
+            _property.Concrete();
+            _something.Generic();
+            _error.Focus();
+            _something.Identifiable();
+        }
+        public void Statement()
+        {
+            _error.Focus();
+        }
+        public void Distributed()
+        {
+            _something.Identifiable();
+            _model.Interface();
+            _singleton.Abstract();
+        }
+    }
+    public class SomethingElse
+    {
+        Visitor _visitor = new Visitor();
+        public void Stateless()
+        {
+            _visitor.Session();
+        }
+    }
+    public class Error
+    {
+        Wrapper _wrapper = new Wrapper();
+        Error _error = new Error();
+        Configuration _configuration = new Configuration();
+        Value _value = new Value();
+        public void Focus()
+        {
+            _wrapper.Iterable();
+            _error.Aspect();
+        }
+        public void Aspect()
+        {
+            _configuration.Aware();
+        }
+        public void Notification()
+        {
+        }
+        public void Jdbc()
+        {
+            _configuration.Distributed();
+            _value.Simple();
+        }
+    }
+    public class Property
+    {
+        Visitor _visitor = new Visitor();
+        Identifier _identifier = new Identifier();
+        Method _method = new Method();
+        public void Transformer()
+        {
+            _visitor.Session();
+        }
+        public void Concrete()
+        {
+        }
+        public void Meta()
+        {
+            _visitor.Principal();
+            _identifier.Interruptible();
+            _method.Scope();
+        }
+    }
+    public class Value
+    {
+        Wrapper _wrapper = new Wrapper();
+        Value _value = new Value();
+        public void Simple()
+        {
+            _wrapper.Prepared();
+            _value.Simple();
+        }
+    }
+    public class Identifier
+    {
+        Error _error = new Error();
+        Wrapper _wrapper = new Wrapper();
+        Singleton _singleton = new Singleton();
+        Something _something = new Something();
+        Property _property = new Property();
+        Visitor _visitor = new Visitor();
+        Identifier _identifier = new Identifier();
+        SomethingElse _somethingElse = new SomethingElse();
+        Model _model = new Model();
+        Method _method = new Method();
+        Configuration _configuration = new Configuration();
+        public void Invalid()
+        {
+            _error.Aspect();
+            _wrapper.Iterable();
+            _singleton.Failure();
+            _something.Generic();
+        }
+        public void Traversal()
+        {
+            _property.Meta();
+            _visitor.Autowire();
+            _identifier.Type();
+        }
+        public void Common()
+        {
+            _somethingElse.Stateless();
+        }
+        public void Driven()
+        {
+            _wrapper.Advisable();
+            _model.Interface();
+        }
+        public void Interruptible()
+        {
+            _wrapper.Advisable();
+            _method.Transaction();
+            _configuration.Supported();
+        }
+        public void Remote()
+        {
+            _method.Literal();
+            _model.Observable();
+            _identifier.Remote();
+        }
+        public void Type()
+        {
+            _identifier.Invalid();
+        }
+    }
+    public class Something
+    {
+        Configuration _configuration = new Configuration();
+        Something _something = new Something();
+        Bean _bean = new Bean();
+        public void Transactional()
+        {
+            _configuration.Distributed();
+            _configuration.Distributed();
+        }
+        public void Generic()
+        {
+            _configuration.Aware();
+        }
+        public void Identifiable()
+        {
+            _something.Identifiable();
+            _something.Identifiable();
+            _bean.Batch();
+        }
+    }
+    public class Authentication
+    {
+    }
+
+}
---
+++ SpaghettiGenerator/Program.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaghettiGenerator
+{
+    class Program
+    {
+        static readonly Random random = new Random();
+
+        static readonly List<string> ClassNames = new List<string>()
+        {
+            "Factory", "Bean", "Wrapper", "Visitor", "Model", "Singleton",
+            "AThing", "Configuration", "SomethingElse", "Error", "Property", "Value",
+            "Identifier", "Something", "Authentication"
+        };
+
+        static readonly List<string> MethodNames = new List<string>()
+        {
+            "Composite", "Invalid", "Supported", "Focus", "Traversal", "Abstract",
+            "Transformer", "Common", "Concrete", "Autowire", "Simple", "Aware",
+            "Aspect", "Principal", "Driven", "Interruptible", "Batch",
+            "Prepared", "Statement", "Remote", "Stateless", "Session",
+            "Transaction", "Transactional", "Based", "Meta", "Data", "Jms",
+            "Readable", "Literal", "Reflective", "Scope", "Multipart", "Xml",
+            "Generic", "Interface", "Advisable", "Observable", "Identifiable",
+            "Iterable", "Distributed", "Notification", "Failure", "Type",
+            "Http", "Jdbc"
+        };
+
+        private static readonly IList<AThing> Methods = MethodNames.Select(x => new AThing(x)).ToList();
+
+        private static readonly IList<Class> Classes = ClassNames.Select(x => new Class(x)).ToList();
+
+        static void Main(string[] args)
+        {
+
+            foreach (var method in Methods)
+            {
+                for (var i = 0; i < random.Next(0, 6); i++)
+                {
+                    method.Calls(RandomMethod());
+                }
+
+                method.BelongsTo(RandomClass());
+            }
+
+            var sb = new StringBuilder();
+            foreach (var @class in Classes)
+            {
+                @class.SerialiseTo(x => sb.AppendLine(x));
+            }
+            Console.Write(sb.ToString());
+
+            Console.ReadLine();
+        }
+
+        private static AThing RandomMethod()
+        {
+            return Random(Methods);
+        }
+
+        private static Class RandomClass()
+        {
+            return Random(Classes);
+        }
+
+        private static T Random<T>(IEnumerable<T> ts)
+        {
+            var enumerable = ts as T[] ?? ts.ToArray();
+            var index = random.Next(0, enumerable.Count()- 1);
+            return enumerable.ElementAt(index);
+        }
+    }
+
+    public class Class
+    {
+        private readonly IList<AThing> _methods = new List<AThing>();
+        private readonly string _name;
+
+        public Class(string name)
+        {
+            _name = name;
+        }
+
+        public void Add(AThing aThing)
+        {
+            _methods.Add(aThing);
+        }
+
+        public void SerialiseTo(Action<string> append)
+        {
+            append($"public class {_name} {{");
+
+            appendInitialisedFields(append);
+
+            foreach (var method in _methods)
+            {
+                method.SerialiseDefinitionTo(append);
+            }
+            append($"}}");
+
+        }
+
+        private void appendInitialisedFields(Action<string> append)
+        {
+            var classesUsed = _methods.SelectMany(m => m.CalledMethods()).Select(m => m.OwningClass()).Distinct(new ClassComparer());
+            foreach (var @class in classesUsed)
+            {
+                @class.appendInitialisedField(append);
+            }
+        }
+
+        private void appendInitialisedField(Action<string> append)
+        {
+            append($"{_name} {FieldName()} = new {_name}();");
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public string FieldName()
+        {
+            return $"_{_name.ToLower()}";
+        }
+    }
+
+    public class ClassComparer : IEqualityComparer<Class>
+    {
+        public bool Equals(Class x, Class y)
+        {
+            if (x.GetName() == y.GetName())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int GetHashCode(Class obj)
+        {
+            return obj.GetName().GetHashCode();
+        }
+    }
+
+    public  class AThing
+    {
+        private readonly IList<AThing> _methodsToCall = new List<AThing>();
+        private readonly string _name;
+        private Class _owningClass;
+
+        public AThing(string name)
+        {
+            _name = name;
+        }
+
+        internal void Calls(AThing aThingToCall)
+        {
+            _methodsToCall.Add(aThingToCall);
+        }
+
+        public void BelongsTo(Class owningClass)
+        {
+            if(_owningClass != null) throw new Exception();
+            _owningClass = owningClass;
+            owningClass.Add(this);
+        }
+
+        public void SerialiseDefinitionTo(Action<string> append)
+        {
+            append($"public void {_name}() {{");
+            foreach (var method in _methodsToCall)
+            {
+                method.SerlialiseCallTo(append);
+            }
+            append($"}}");
+        }
+
+        private void SerlialiseCallTo(Action<string> append)
+        {
+            append($"{_owningClass.FieldName()}.{_name}();");
+        }
+
+        public Class OwningClass()
+        {
+            return _owningClass;
+        }
+
+        public IEnumerable<AThing> CalledMethods()
+        {
+            return _methodsToCall;
+        }
+    }
+}
+
+
+
