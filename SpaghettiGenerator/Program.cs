ad136471cbe263f54ad144bd4cb56e1e1c3acdaf
unknown <mail@thejimmyp.com>
2018-03-15 09:04:03 +1100

    adds ndepend and first analysis now generates spaghetti as methods not types hand crafted types to be refactorable

--- SpaghettiGenerator/Program.cs
+++ SpaghettiGenerator/Program.cs
@@ -9,13 +9,6 @@ namespace SpaghettiGenerator
     {
         static readonly Random random = new Random();
 
-        static readonly List<string> ClassNames = new List<string>()
-        {
-            "Factory", "Bean", "Wrapper", "Visitor", "Model", "Singleton",
-            "AThing", "Configuration", "SomethingElse", "Error", "Property", "Value",
-            "Identifier", "Something", "Authentication"
-        };
-
         static readonly List<string> MethodNames = new List<string>()
         {
             "Composite", "Invalid", "Supported", "Focus", "Traversal", "Abstract",
@@ -31,8 +24,6 @@ namespace SpaghettiGenerator
 
         private static readonly IList<AThing> Methods = MethodNames.Select(x => new AThing(x)).ToList();
 
-        private static readonly IList<Class> Classes = ClassNames.Select(x => new Class(x)).ToList();
-
         static void Main(string[] args)
         {
 
@@ -42,15 +33,19 @@ namespace SpaghettiGenerator
                 {
                     method.Calls(RandomMethod());
                 }
-
-                method.BelongsTo(RandomClass());
             }
 
             var sb = new StringBuilder();
-            foreach (var @class in Classes)
+
+            sb.AppendLine("public class God {");
+
+            foreach (var method in Methods)
             {
-                @class.SerialiseTo(x => sb.AppendLine(x));
+                method.SerialiseDefinitionTo(x => sb.AppendLine(x));
             }
+
+            sb.AppendLine("}");
+
             Console.Write(sb.ToString());
 
             Console.ReadLine();
@@ -61,11 +56,6 @@ namespace SpaghettiGenerator
             return Random(Methods);
         }
 
-        private static Class RandomClass()
-        {
-            return Random(Classes);
-        }
-
         private static T Random<T>(IEnumerable<T> ts)
         {
             var enumerable = ts as T[] ?? ts.ToArray();
@@ -73,86 +63,11 @@ namespace SpaghettiGenerator
             return enumerable.ElementAt(index);
         }
     }
-
-    public class Class
-    {
-        private readonly IList<AThing> _methods = new List<AThing>();
-        private readonly string _name;
-
-        public Class(string name)
-        {
-            _name = name;
-        }
-
-        public void Add(AThing aThing)
-        {
-            _methods.Add(aThing);
-        }
-
-        public void SerialiseTo(Action<string> append)
-        {
-            append($"public class {_name} {{");
-
-            appendInitialisedFields(append);
-
-            foreach (var method in _methods)
-            {
-                method.SerialiseDefinitionTo(append);
-            }
-            append($"}}");
-
-        }
-
-        private void appendInitialisedFields(Action<string> append)
-        {
-            var classesUsed = _methods.SelectMany(m => m.CalledMethods()).Select(m => m.OwningClass()).Distinct(new ClassComparer());
-            foreach (var @class in classesUsed)
-            {
-                @class.appendInitialisedField(append);
-            }
-        }
-
-        private void appendInitialisedField(Action<string> append)
-        {
-            append($"{_name} {FieldName()} = new {_name}();");
-        }
-
-        public string GetName()
-        {
-            return _name;
-        }
-
-        public string FieldName()
-        {
-            return $"_{_name.ToLower()}";
-        }
-    }
-
-    public class ClassComparer : IEqualityComparer<Class>
-    {
-        public bool Equals(Class x, Class y)
-        {
-            if (x.GetName() == y.GetName())
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        public int GetHashCode(Class obj)
-        {
-            return obj.GetName().GetHashCode();
-        }
-    }
-
+    
     public  class AThing
     {
         private readonly IList<AThing> _methodsToCall = new List<AThing>();
         private readonly string _name;
-        private Class _owningClass;
 
         public AThing(string name)
         {
@@ -162,13 +77,6 @@ namespace SpaghettiGenerator
         internal void Calls(AThing aThingToCall)
         {
             _methodsToCall.Add(aThingToCall);
-        }
-
-        public void BelongsTo(Class owningClass)
-        {
-            if(_owningClass != null) throw new Exception();
-            _owningClass = owningClass;
-            owningClass.Add(this);
         }
 
         public void SerialiseDefinitionTo(Action<string> append)
@@ -183,12 +91,7 @@ namespace SpaghettiGenerator
 
         private void SerlialiseCallTo(Action<string> append)
         {
-            append($"{_owningClass.FieldName()}.{_name}();");
-        }
-
-        public Class OwningClass()
-        {
-            return _owningClass;
+            append($"{_name}();");
         }
 
         public IEnumerable<AThing> CalledMethods()
---
+++ SpaghettiGenerator/Spaghetti.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spaghetti
+{
+    public class God
+    {
+        public void Composite()
+        {
+            Http();
+            Aware();
+            Stateless();
+        }
+        public void Invalid()
+        {
+            Principal();
+            Stateless();
+        }
+        public void Supported()
+        {
+            Aware();
+            Invalid();
+            Stateless();
+        }
+        public void Focus()
+        {
+            Iterable();
+            Stateless();
+        }
+        public void Traversal()
+        {
+            Autowire();
+            Principal();
+            Stateless();
+        }
+        public void Abstract()
+        {
+            Aspect();
+            Simple();
+            Stateless();
+        }
+        public void Transformer()
+        {
+            Stateless();
+        }
+        public void Common()
+        {
+            Driven();
+            Simple();
+            Stateless();
+        }
+        public void Concrete()
+        {
+            Stateless();
+        }
+        public void Autowire()
+        {
+            Abstract();
+            Stateless();
+            Jms();
+        }
+        public void Simple()
+        {
+            Based();
+            Stateless();
+        }
+        public void Aware()
+        {
+            Prepared();
+            Autowire();
+            Scope();
+            Stateless();
+        }
+        public void Aspect()
+        {
+            Iterable();
+            Stateless();
+        }
+        public void Principal()
+        {
+            Stateless();
+            Iterable();
+        }
+        public void Driven()
+        {
+            Stateless();
+        }
+        public void Interruptible()
+        {
+            Stateless();
+        }
+        public void Batch()
+        {
+            Stateless();
+        }
+        public void Prepared()
+        {
+            Http();
+            Stateless();
+            Driven();
+            Based();
+        }
+        public void Statement()
+        {
+            Xml();
+            Stateless();
+            Driven();
+            Type();
+        }
+        public void Remote()
+        {
+            Jms();
+            Http();
+            Stateless();
+        }
+        public void Stateless()
+        {
+            Transaction();
+            Observable();
+        }
+        public void Session()
+        {
+            Scope();
+            Generic();
+            Stateless();
+        }
+        public void Transaction()
+        {
+        }
+        public void Transactional()
+        {
+            Aware();
+            Stateless();
+        }
+        public void Based()
+        {
+            Reflective();
+            Principal();
+            Driven();
+        }
+        public void Meta()
+        {
+            Abstract();
+            Concrete();
+            Stateless();
+        }
+        public void Data()
+        {
+            Batch();
+            Stateless();
+        }
+        public void Jms()
+        {
+            Stateless();
+        }
+        public void Readable()
+        {
+        }
+        public void Literal()
+        {
+            Meta();
+        }
+        public void Reflective()
+        {
+            Stateless();
+        }
+        public void Scope()
+        {
+            Data();
+        }
+        public void Multipart()
+        {
+            Identifiable();
+            Stateless();
+        }
+        public void Xml()
+        {
+            Common();
+            Multipart();
+            Stateless();
+        }
+        public void Generic()
+        {
+            Failure();
+            Stateless();
+        }
+        public void Interface()
+        {
+            Readable();
+            Stateless();
+        }
+        public void Advisable()
+        {
+            Prepared();
+            Stateless();
+        }
+        public void Observable()
+        {
+            Readable();
+            Transaction();
+        }
+        public void Identifiable()
+        {
+            Focus();
+            Literal();
+            Stateless();
+        }
+        public void Iterable()
+        {
+            Stateless();
+        }
+        public void Distributed()
+        {
+            Autowire();
+            Batch();
+        }
+        public void Notification()
+        {
+            Stateless();
+        }
+        public void Failure()
+        {
+            Stateless();
+        }
+        public void Type()
+        {
+            Simple();
+            Stateless();
+        }
+        public void Http()
+        {
+            Readable();
+            Jms();
+            Principal();
+        }
+        public void Jdbc()
+        {
+            Stateless();
+        }
+    }
+}
